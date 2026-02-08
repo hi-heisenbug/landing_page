a906450d7428e66e06f06ce8e897839754bf6001
Saurabh Gupta <saurabh@saurabh>
2026-02-08 23:27:21 +0530

    42

---
+++ components/footer.tsx
@@ -0,0 +1,23 @@
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { buttonVariants } from "./ui/button";
+import XLogoIcon from "./icons/x";
+import LinkedInLogoIcon from "./icons/linkedin";
+import { socialLinks } from "@/lib/constants";
+import Link from "next/link";
+
+export const Footer = () => {
+  return (
+    <div className="flex gap-6 items-center absolute bottom-[calc(var(--inset)+0.8rem)] md:bottom-[calc(var(--inset)+1.5rem)] left-1/2 -translate-x-1/2">
+      <Link target="_blank" className={buttonVariants({ size: "icon-xl" })} href={socialLinks.linkedin}>
+        <LinkedInLogoIcon className="size-6" />
+      </Link>
+      <Link target="_blank" className={buttonVariants({ size: "icon-xl" })} href={socialLinks.x}>
+        <XLogoIcon className="size-6" />
+      </Link>
+      <Link target="_blank" className={buttonVariants({ size: "icon-xl" })} href={socialLinks.github}>
+        <GitHubLogoIcon className="size-6" />
+      </Link>
+    </div>
+  );
+};
+
