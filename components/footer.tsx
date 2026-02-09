c9605a297316c4735ec0dd6022eb1f9c765c3ec2
Saurabh Gupta <saurabh@saurabh>
2026-02-09 18:22:14 +0530

    v0

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,20 +2,28 @@ import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { buttonVariants } from "./ui/button";
 import XLogoIcon from "./icons/x";
 import LinkedInLogoIcon from "./icons/linkedin";
+import InstagramLogoIcon from "./icons/instagram";
+import YouTubeLogoIcon from "./icons/youtube";
 import { socialLinks } from "@/lib/constants";
 import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <div className="flex gap-6 items-center absolute bottom-[calc(var(--inset)+0.8rem)] md:bottom-[calc(var(--inset)+1.5rem)] left-1/2 -translate-x-1/2">
+    <div className="flex gap-4 items-center absolute bottom-[calc(var(--inset)+0.8rem)] md:bottom-[calc(var(--inset)+1.5rem)] left-1/2 -translate-x-1/2">
       <Link target="_blank" className={buttonVariants({ size: "icon-xl" })} href={socialLinks.linkedin}>
-        <LinkedInLogoIcon className="size-6" />
+        <LinkedInLogoIcon className="size-5" />
       </Link>
       <Link target="_blank" className={buttonVariants({ size: "icon-xl" })} href={socialLinks.x}>
-        <XLogoIcon className="size-6" />
+        <XLogoIcon className="size-5" />
       </Link>
       <Link target="_blank" className={buttonVariants({ size: "icon-xl" })} href={socialLinks.github}>
-        <GitHubLogoIcon className="size-6" />
+        <GitHubLogoIcon className="size-5" />
+      </Link>
+      <Link target="_blank" className={buttonVariants({ size: "icon-xl" })} href={socialLinks.instagram}>
+        <InstagramLogoIcon className="size-5" />
+      </Link>
+      <Link target="_blank" className={buttonVariants({ size: "icon-xl" })} href={socialLinks.youtube}>
+        <YouTubeLogoIcon className="size-5" />
       </Link>
     </div>
   );
