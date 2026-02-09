c1dd60ffdcb830faee3f2fa4da7319d0a47afa41
Saurabh Gupta <saurabh@saurabh>
2026-02-09 18:52:28 +0530

    v1.1

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="p-inset h-[100dvh] w-full">
       <div className="relative h-full w-full">
-        <Background src="/background.gif" />
+        <Background src="/background.mp4" />
         <Newsletter />
         <Footer />
       </div>
